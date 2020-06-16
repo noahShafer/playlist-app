da186a6e1ac41085d4d46074f6db596206eb83b6
noahShafer <noahshafer0211@gmail.com>
2020-06-16 16:29:45 -0500

    Shortened dev script call

--- playlist-app-frontend/src/app.js
+++ playlist-app-frontend/src/app.js
@@ -1,11 +1,11 @@
-import Navbar from "./components/Navbar.js";
+import navbar from "./components/Navbar.js";
 import Router from "./Router.js";
 
 
 const BASE_URL = "http://localhost:3000";
 
 window.addEventListener('load', e => {
-    document.querySelector('html').innerHTML = Navbar;
+    document.querySelector('html').innerHTML = navbar;
     document.querySelector('head').innerHTML += '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">'
 
     document.querySelector('#login-btn').addEventListener('click', e => {
